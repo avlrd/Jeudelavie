6235605e0246e3efa7290dc24e63629c46a6ce58
Arthur Villard <arthur.villard@etu.unistra.fr>
2020-10-17 16:18:07 +0200

    Organisation

---
+++ src/io.c
@@ -0,0 +1,130 @@
+/**
+ * 	\file io.C
+ *	fonctions d'entrée et sortie
+ *	\author Arthur Villard
+*/
+
+#include "io.h"
+#include <stdbool.h>
+
+bool cyclique = true;
+bool vieillissement = false;
+
+/**
+ *	\fn void affiche_trait (int c)
+ *	\param c nombre de colonnes
+ *	\brief affichage d'un trait horizontal
+*/
+void affiche_trait (int c){
+	int i;
+	for (i=0; i<c; ++i) printf ("|---");
+	printf("|\n");
+	return;
+}
+
+/**
+ *	\fn void affiche_ligne (int c, int* ligne)
+ *	\param c nombre de colonnes
+ *	\param ligne pointeur vers un tableau
+ *	\brief affichage d'une ligne de la grille
+*/
+
+void affiche_ligne (int c, int* ligne){
+	int i;
+	for (i=0; i<c; ++i) 
+		if (ligne[i] == 0 ) printf ("|   "); else printf ("| O ");
+	printf("|\n");
+	return;
+}
+
+/**
+ *	\fn void affiche_grille (grille g)
+ *	\param g une grille
+ *	\brief affichage d'une grille
+*/
+
+void affiche_grille (grille g){
+	int i, l=g.nbl, c=g.nbc;
+	printf("\n");
+	affiche_trait(c);
+	for (i=0; i<l; ++i) {
+		affiche_ligne(c, g.cellules[i]);
+		affiche_trait(c);
+	}	
+	printf("\n"); 
+	return;
+}
+
+/**
+ *	\fn void efface_grille (grille g)
+ *	\param g une grille
+ *	\brief effacement d'une grille
+*/
+
+void efface_grille (grille g){
+	printf("\n\e[%dA",g.nbl*2 + 5); 
+}
+
+/**
+ *	\fn void debut_jeu(grille *g, grille *gc)
+ *	\param g pointeur sur une grille
+ *	\param gc pointeur sur une grille (copie)
+ *	\brief fonction qui débute le jeu
+*/
+
+void debut_jeu(grille *g, grille *gc){
+	char c = getchar(); 
+	while (c != 'q') // touche 'q' pour quitter
+	{ 
+		switch (c) {
+			case '\n' : 
+			{ // touche "entree" pour évoluer
+				if(cyclique)
+					evolue(g,gc, vieillissement, &compte_voisins_vivants);
+				else
+					evolue(g,gc, vieillissement, &compte_voisins_vivants_nc);					
+				efface_grille(*g);
+				affiche_grille(*g);
+				break;
+			}
+
+			case 'n' :
+			{ // touche "n" pour créer une nouvelle grille
+
+				char newgrille[30];
+
+				printf("Entrez le nom de la nouvelle grille :");
+				scanf("%s", &newgrille);
+				printf("%s", newgrille);
+				libere_grille(g);
+				libere_grille(gc);
+
+				init_grille_from_file(newgrille, g);
+
+				alloue_grille(g->nbl, g->nbc, gc);
+				affiche_grille(*g);
+				break;
+			}
+
+			case 'c' :
+			{ // touche c pour alterner entre mode cyclique et non cyclique
+				cyclique = !(cyclique);
+				break;
+			}
+
+			case 'v' :
+			{ // touche v pour activer / désactiver le vieillissement
+				vieillissement = !(vieillissement);
+				break;
+			}
+
+			default : 
+			{ // touche non traitée
+				printf("\n\e[1A");
+				break;
+			}
+		}
+		c = getchar(); 
+	}
+	return;	
+}
---
+++ src/jeu.c
@@ -0,0 +1,88 @@
+/**
+ *	\file jeu.c
+ *	fonctionnement du jeu
+ *	\author Arthur Villard
+*/
+
+
+#include "jeu.h"
+#include <stdbool.h>
+
+extern bool cyclique;
+
+/**
+ *	\fn int compte_voisins_vivants_nc(int i, int j, grille g)
+ *	\param i entier
+ *	\param j entier
+ *	\param g une grille
+ *	\brief compte le nombre de voisins vivants de façon cyclique
+*/
+
+int compte_voisins_vivants (int i, int j, grille g){
+	int v = 0, l=g.nbl, c = g.nbc;
+	v+= est_vivante(modulo(i-1,l),modulo(j-1,c),g);
+	v+= est_vivante(modulo(i-1,l),modulo(j,c),g);
+	v+= est_vivante(modulo(i-1,l),modulo(j+1,c),g);
+	v+= est_vivante(modulo(i,l),modulo(j-1,c),g);
+	v+= est_vivante(modulo(i,l),modulo(j+1,c),g);
+	v+= est_vivante(modulo(i+1,l),modulo(j-1,c),g);
+	v+= est_vivante(modulo(i+1,l),modulo(j,c),g);
+	v+= est_vivante(modulo(i+1,l),modulo(j+1,c),g);
+
+	return v; 
+}
+
+/**
+ *	\fn int compte_voisins_vivants_nc(int i, int j, grille g)
+ *	\param i entier
+ *	\param j entier
+ *	\param g une grille
+ *	\brief compte le nombre de voisins vivants de façon non cyclique
+*/
+
+int compte_voisins_vivants_nc(int i, int j, grille g){
+	int v=0, l=g.nbl, c = g.nbc;
+
+		v+= (i>0&&j>0) ? est_vivante(i-1,j-1,g) : 0;
+		v+= (i>0) ? est_vivante(i-1,j,g) : 0;
+		v+= (i>0&&j>c-1) ? est_vivante(i-1,j+1,g) : 0;
+		v+= (j>0) ? est_vivante(i,j-1,g) : 0;
+		v+= (j<c-1) ? est_vivante(i,j+1,g) : 0;
+		v+= (i<l-1&&j>0) ? est_vivante(i+1,j-1,g) : 0;
+		v+= (i<l-1) ? est_vivante(i+1,j,g) : 0;
+		v+= (i<l-1&&j<c-1) ? est_vivante(i+1,j+1,g) : 0;
+
+	
+	return v; 
+}
+
+/** 
+ *	\fn void evolue(grille *g, grille *gc)
+ *	\param g pointeur vers une grille
+ *	\param gc pointeur vers une grille (copie)
+ *	\brief fait évoluer la grille g d'un pas dans le temps
+ */
+
+void evolue (grille *g, grille *gc, int vieillissement, int (*fonction) (int, int, grille))
+{
+	copie_grille (*g,*gc); // copie temporaire de la grille
+	int i,j,l=g->nbl, c = g->nbc,v;
+	for (i=0; i<l; i++)
+	{
+		for (j=0; j<c; ++j)
+		{
+			v = fonction(i, j, *gc);
+			if (est_vivante(i,j,*gc)) 
+			{ // evolution d'une cellule vivante
+				if ((g->cellules[i][j] >= 8 && vieillissement) || (v!=2 && v!=3)) set_morte(i,j,*g);
+
+				else g->cellules[i][j]= g->cellules[i][j]+ 1;
+			}
+			else 
+			{ // evolution d'une cellule morte
+				if ( v==3 ) set_vivante(i,j,*g);
+			}
+		}
+	}
+	return;
+}
